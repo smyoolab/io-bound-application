2d21afd94d6ff4dd2f60ae6fd712edc962c5e698
smyoo <sangmanyoo@gmail.com>
2021-04-29 23:24:07 +0900

    feat: 글 번호로 조회 기능 추가

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -22,19 +22,22 @@ public class PostController {
         return postRepository.save(post);
     }
 
-    // 2-1. 글 목록을 조회한다.
-    // 2-2 글 목록을 페이징하여 반환
+    // 2. 글 목록을 페이징하여 반환
     @GetMapping("/posts")
     public Page<Post> getPostList(@RequestParam(defaultValue = "1") Integer page) {
         return postRepository.findAll(
                 PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
         );
     }
-    
 
-    
     // 3. 글 번호로 조회
-    
+    @GetMapping("/post/{id}")
+    public Post getPostById(@PathVariable("id") Long id){
+        return postRepository.findById(id).get();
+    }
+
+
+
     // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
 
 }
